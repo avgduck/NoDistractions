6ac3232ceecc8dc8881945fa2da2c35b4a3a69a4
avgduck <avgduck@gmail.com>
2025-11-09 17:48:07 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace NoDistractions;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(PluginDetails.DEPENDENCY_LLBML, BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(PluginDetails.DEPENDENCY_MODMENU, BepInDependency.DependencyFlags.SoftDependency)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; private set; }
+    internal static ManualLogSource LogGlobal { get; private set; }
+
+    private void Awake()
+    {
+        LogGlobal = base.Logger;
+        LogGlobal.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Start()
+    {
+        LLBML.Utils.ModDependenciesUtils.RegisterToModMenu(this.Info, PluginDetails.MODMENU_TEXT);
+    }
+}
---
+++ PluginDetails.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NoDistractions;
+
+public class PluginDetails
+{
+    internal const string DEPENDENCY_LLBML = "fr.glomzubuk.plugins.llb.llbml";
+    internal const string DEPENDENCY_MODMENU = "no.mrgentle.plugins.llb.modmenu";
+
+    internal static readonly List<string> MODMENU_TEXT = [
+        "Disables stage distractions"
+    ];
+}
