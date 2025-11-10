d06f946fdfcbdeec0da610e882ed36e3ae950c38
avgduck <avgduck@gmail.com>
2025-11-10 03:19:49 -0500

    Fix plugin GUID

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -3,7 +3,7 @@ using BepInEx.Logging;
 
 namespace NoStageEffects;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin("avgduck.plugins.llb.nostageeffects", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     private void Awake()
