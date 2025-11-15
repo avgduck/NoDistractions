250a3e12ff03495b14c2c9b5c3b4f0b5924fda0f
avgduck <avgduck@gmail.com>
2025-11-15 16:06:04 -0500

    Make GUID a public field

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -3,11 +3,12 @@ using BepInEx.Logging;
 
 namespace NoStageEffects;
 
-[BepInPlugin("avgduck.plugins.llb.nostageeffects", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("fr.glomzubuk.plugins.llb.llbml", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency("no.mrgentle.plugins.llb.modmenu", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
+    public const string GUID = "avgduck.plugins.llb.nostageeffects";
     public static Plugin Instance { get; private set; }
     internal static ManualLogSource LogGlobal { get; private set; }
     private void Awake()
